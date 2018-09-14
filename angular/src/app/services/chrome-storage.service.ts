1b5babe495e05435131dd0a8d92c00d53eaa0df3
Igor Kurilo <igor.kurilo31@gmail.com>
2018-09-14 16:10:31 +0300

    generate key pair hes been changed

--- angular/src/app/services/chrome-storage.service.ts
+++ angular/src/app/services/chrome-storage.service.ts
@@ -8,47 +8,20 @@ export class ChromeStorageService {
   constructor() { }
 
   public save(key, value) {
-    this.chrome.storage.local.set({[key]: value}, () => {
-
-    });
+    this.chrome.storage.local.set({[key]: value}, () => { });
   }
 
   public load(key) {
     this.chrome.storage.local.get(key, (result) => {
-      alert(JSON.stringify(result))
       return result;
     });
   }
 
   public remove(key) {
-    this.chrome.storage.local.remove(key, () => {
-
-    });
+    this.chrome.storage.local.remove(key, () => { });
   }
 
   public clear() {
-    this.chrome.storage.local.clear(() => {
-
-    });
+    this.chrome.storage.local.clear(() => { });
   }
-  // ----------
-  // public load(key, cbk) {
-  //   try {
-  //     this.chrome.storage.local.get([key], (res) => {
-  //       cbk(res, null);
-  //     });
-  //   } catch (e) {
-  //     cbk(null, e);
-  //   }
-  // }
-  //
-  // public save(key, value, cbk) {
-  //   try {
-  //     this.chrome.storage.local.set({key: value}, () => {
-  //       cbk(true, null);
-  //     });
-  //   } catch (e) {
-  //     cbk(false, e);
-  //   }
-  // }
 }
--- angular/src/app/services/generate-wallet.service.ts
+++ angular/src/app/services/generate-wallet.service.ts
@@ -1,66 +1,28 @@
 import { Injectable } from '@angular/core';
-import * as nacl from "../../assets/sumus/nacl.js"
-import * as bs58 from 'bs58';
-import * as CRC32 from 'crc-32';
 import * as CryptoJS from 'crypto-js';
-import {MessageBoxService} from "./message-box.service";
+import * as sumus from '../../../../sumuslib/sumuslib.js';
 
 @Injectable()
 export class GenerateWalletService {
 
-  private privateKey;
-  private publicKey;
+  private sumusLib = window['SumusLib']
   private encryptedKey;
 
-  constructor(private messageBox: MessageBoxService) { }
+  constructor() {
+    sumus;
+  }
 
   getPublicKeyFromPrivate(key: string) {
-    let bytes;
-    try {
-      bytes = bs58.decode(key);
-    } catch (e) {
-      this.messageBox.alert('Something went wrong');
-    }
-
-    if (bytes && (bytes.length > 4 || key.length > 4)) {
-      let value = nacl.sign.keyPair.publicFromPrehashedSecretKey(bytes.slice(0, -4));
-      let privateCrc32 = CRC32.buf(value);
-
-      let publicHashsum = [];
-      for (let i = 0; i <= 24; i += 8) {
-        publicHashsum.push((privateCrc32 >> i) & 255);
-      }
-      let publicKey = bs58.encode(Array.from(value).concat(publicHashsum));
-
-      return publicKey;
-    } else {
-      this.messageBox.alert('Something went wrong');
-    }
+    let publicKey = this.sumusLib.Signer.FromPK(key).PublicKey();
+    return publicKey;
   }
 
   createWallet(identify) {
-    let pair = nacl.sign.keyPair();
-    let pkPrehashed = nacl.sign.keyPair.prehashSecretKey(pair.secretKey);
-
-    const privateCrc32 = CRC32.buf(pkPrehashed);
-    let privateHashsum = [];
-    for (let i = 0; i <= 24; i += 8) {
-      privateHashsum.push((privateCrc32 >> i) & 255);
-    }
-
-    const publicCrc32 = CRC32.buf(nacl.sign.keyPair.publicFromPrehashedSecretKey(pkPrehashed));
-    let publicHashsum = [];
-    for (let i = 0; i <= 24; i += 8) {
-      publicHashsum.push((publicCrc32 >> i) & 255);
-    }
-
-    this.privateKey = bs58.encode(Array.from(pkPrehashed).concat(privateHashsum));
-    this.publicKey = bs58.encode(Array.from(nacl.sign.keyPair.publicFromPrehashedSecretKey(pkPrehashed)).concat(publicHashsum));
-
-    this.encryptedKey = CryptoJS.AES.encrypt(this.privateKey, identify).toString();
+    let sig = this.sumusLib.Signer.Generate();
+    this.encryptedKey = CryptoJS.AES.encrypt(sig.PrivateKey(), identify).toString();
 
     const keys = {
-      publicKey: this.publicKey,
+      publicKey: sig.PublicKey(),
       encryptedKey: this.encryptedKey
     }
 
