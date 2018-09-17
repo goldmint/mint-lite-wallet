5c10d421e2c2fff73b9d975f4a624af0a02fa28d
Igor Kurilo <igor.kurilo31@gmail.com>
2018-09-17 15:22:54 +0300

    changed enviroment link

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://staging.goldmint.io/wallet/api/v1',
   detailsTxInfoLink: 'https://staging.goldmint.io/wallet/#/scanner/tx/',
   webWallet: 'https://staging.goldmint.io/wallet/#/',
-  sumusProxyUrl: 'https://service.goldmint.io/sumus/rest-proxy/cmd/',
+  sumusProxyUrl: 'https://service.goldmint.io/sumus/rest-proxy/cmd',
   timeTxFailed: 1800000 // 30 minutes
 };
