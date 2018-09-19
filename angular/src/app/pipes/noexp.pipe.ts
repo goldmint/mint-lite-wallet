08893fffc7b725cc775a39f1b74b551f599659f7
Igor Kurilo <igor.kurilo31@gmail.com>
2018-09-19 19:51:01 +0300

    changed noexp pipe

--- angular/src/app/pipes/noexp.pipe.ts
+++ angular/src/app/pipes/noexp.pipe.ts
@@ -3,6 +3,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'noexp'})
 export class NoexpPipe implements PipeTransform {
   transform(value: number) {
+    const amount = this.getNoExpValue(value);
+    const position = amount.toString().indexOf('.');
+    if (position >= 0) {
+      return amount.toString().substr(0, position + 9);
+    } else {
+      return amount;
+    }
+  }
+
+  getNoExpValue(value) {
     let data = String(value).split(/[eE]/);
     if(data.length== 1) return data[0];
 
