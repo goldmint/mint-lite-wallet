ff7726667bc2c4a5cea89a5356813643e55d0dfa
Igor Kurilo <igor.kurilo31@gmail.com>
2018-09-28 20:59:42 +0300

    get balance api method in content.js has been added

--- angular/src/content.js
+++ angular/src/content.js
@@ -1,5 +1,11 @@
 "use strict"
 
+const config = {
+    api: {
+        getBalance: 'https://service.goldmint.io/sumus/rest-proxy/v1/wallet/'
+    }
+};
+
 function injectScript(file_path, tag) {
     var node = document.getElementsByTagName(tag)[0];
     var script = document.createElement('script');
@@ -27,21 +33,52 @@ window.addEventListener("message", (data) => {
             resourceData = JSON.parse(data.data.data);
         } catch (e) {}
 
-        actions[data.data.resource](resourceData).then(r =>
-        {
+        actions[data.data.resource](resourceData).then(r => {
             window.postMessage({type: 'answer', id: data.data.id, isSuccess: true, data: r}, "*");
-        }, r =>
-        {
+        }, r => {
             window.postMessage({type: 'answer', id: data.data.id, isSuccess: false, data: r}, "*");
         });
     }
 });
 
+function http(method, url, params = '') {
+    let xhr = new XMLHttpRequest(),
+        currentUrl = method.toUpperCase() === "GET" ? url + params : url;
+
+    xhr.open(method.toUpperCase(), currentUrl, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    method.toUpperCase() === "GET" ? xhr.send() : xhr.send(params);
+
+    return new Promise((resolve, reject) => {
+        xhr.onload = () => {
+            try {
+                resolve(JSON.parse(xhr.responseText));
+            } catch (e) {
+                resolve(null);
+            }
+        };
+        xhr.onerror = () => {
+            resolve(null);
+        }
+    });
+}
+
 var actions = {
     getAccount: data => new Promise((resolve, reject) => {
-        browser.storage.local.get(null, (result) =>
-        {
+        browser.storage.local.get(null, (result) => {
             resolve(isLoggedIn ? [result.wallets[result.currentWallet].publicKey] : []);
+        });
+    }),
+    getBalance: data => new Promise((resolve, reject) => {
+        http('GET', config.api.getBalance, data.address).then(result => {
+            if (result) {
+                resolve({
+                    gold: result.res.balance.gold,
+                    mint: result.res.balance.mint
+                });
+            } else {
+                resolve(null);
+            }
         });
     }),
 };
--- angular/src/inpage.js
+++ angular/src/inpage.js
@@ -19,50 +19,12 @@
 
         constructor() { }
 
-        // getBalance() {
-        //     let xhr = new XMLHttpRequest();
-        //     let isLoggedIn = false;
-        //
-        //     browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        //         if (request.hasOwnProperty('loginStatus')) {
-        //             isLoggedIn = request.loginStatus;
-        //
-        //             return new Promise((resolve, reject) => {
-        //                 if (isLoggedIn) {
-        //                     browser.storage.local.get(null, (result) => {
-        //                         const currentWallet = result.currentWallet;
-        //                         const walletList = result.wallets;
-        //
-        //                         xhr.open('GET', config.api.getBalance + walletList[currentWallet].publicKey, true);
-        //                         xhr.setRequestHeader('Content-Type', 'application/json');
-        //                         xhr.send();
-        //                         xhr.onload = () => {
-        //                             try {
-        //                                 const result = JSON.parse(xhr.responseText);
-        //                                 resolve({
-        //                                     gold: result.res.balance.gold,
-        //                                     mnt: result.res.balance.mint
-        //                                 });
-        //                             } catch (e) {
-        //                                 resolve(null);
-        //                             }
-        //                         };
-        //                         xhr.onerror = () => {
-        //                             resolve(null);
-        //                         }
-        //                     });
-        //                 } else {
-        //                     resolve(null);
-        //                 }
-        //             });
-        //         }
-        //     });
-        //
-        //     browser.runtime.sendMessage({checkLoginStatus: true});
-        // }
-
         getAccount() {
             return sendQuestion('getAccount');
+        }
+
+        getBalance(address) {
+            return sendQuestion('getBalance', {address});
         }
     }
 
