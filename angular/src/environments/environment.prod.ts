51dfc2cf49da18b4dd928f3df1f0902a9f6d36f2
Vasiliy R <vixpalg@gmail.com>
2020-04-05 13:29:51 +0300

    address approvement page; scanner link; version update;

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  webWallet: 'https://app.cyberbridge.ee/#/scanner/address/',
+  webWallet: 'https://scanner.goldmint.io/#/scanner/address/',
   networkUrl: {
     main: 'https://service.goldmint.io/mint/mainnet/v1',
     test: 'https://service.goldmint.io/mint/testnet/v1'
   },
   rateUrl: 'https://service.goldmint.io/info/rate/v1',
-  detailsTxInfoLink: 'https://app.cyberbridge.ee/#/scanner/tx/',
+  detailsTxInfoLink: 'https://scanner.goldmint.io/#/scanner/tx/',
   timeTxFailed: 1800000, // 30 minutes,
   backupVersion: 2
 };
