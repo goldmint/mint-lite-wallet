63f46d8b49316e258c37962d51441d21658233b5
Vasiliy R <vixpalg@gmail.com>
2020-04-05 15:50:47 +0300

    links are changed

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
-  webWallet: 'https://scanner.goldmint.io/#/scanner/address/',
+  webWallet: 'https://app.cyberbridge.ee/#/scanner/address/',
   networkUrl: {
     main: 'https://service.goldmint.io/mint/mainnet/v1',
     test: 'https://service.goldmint.io/mint/testnet/v1'
   },
   rateUrl: 'https://service.goldmint.io/info/rate/v1',
-  detailsTxInfoLink: 'https://scanner.goldmint.io/#/scanner/tx/',
+  detailsTxInfoLink: 'https://app.cyberbridge.ee/#/scanner/tx/',
   timeTxFailed: 1800000, // 30 minutes,
   backupVersion: 2
 };
